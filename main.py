4b053d6aada5e6dddc60b316588c632a9400f461
mauriciomuniz <mauricio.santana2010@hotmail.com>
2023-04-01 20:39:05 -0300

    Update main.py
    atualizando main

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import Car
 import Broker
+from Eletric_station import EletricStation
+import threading
+
 
 if __name__ == "__main__":
 
     bk = Broker.BrokerSRV('localhost','bk1' ,1883)
 
-    cl = Car.Client("car1", 'localhost', 1883).send_msg()
+    # eletric_station = EletricStation()
+    cl = Car.Client("car1", 'localhost', 1883,100)
+    
+    cl_thread = threading.Thread(target=cl.send_msg)
+    cl_thread.start()
